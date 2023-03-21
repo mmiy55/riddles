800cf18df515e66f8c7809edb6146d6b492146d3
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-21 14:44:42 +0900

    leaves only read action available

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -5,25 +5,25 @@ class RiddlesController < ApplicationController
   end
 
   def show
-    @riddle = Riddle.find(params[:id].to_i)
+    @riddle = Riddle.find(params[:id])
     render json: @riddle
   end
 
-  def create
-    @riddle = Riddle.create(title: params[:name], question: params[:question], answer: params[:answer])
-    render json: @riddle
-  end
+  # def create
+  #   @riddle = Riddle.create(title: params[:title], question: params[:question], answer: params[:answer])
+  #   render json: @riddle
+  # end
 
-  def update
-    @riddle = Riddle.find(params[:id])
-    @riddle.update(title: params[:name], question: params[:question], answer: params[:answer])
-    render json: "#{@riddle.title} has been updated!"
-  end
+  # def update
+  #   @riddle = Riddle.find(params[:id])
+  #   @riddle.update(title: params[:title], question: params[:question], answer: params[:answer])
+  #   render json: "#{@riddle.title} has been updated!"
+  # end
 
-  def destroy
-    @riddle = Riddle.find(params[:id])
-    @riddle.destroy
-    render json: "#{@riddle.title} has been deleted!"
-  end
+  # def destroy
+  #   @riddle = Riddle.find(params[:id])
+  #   @riddle.destroy
+  #   render json: "#{@riddle.title} has been deleted!"
+  # end
 
 end
