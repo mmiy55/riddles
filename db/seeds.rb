f92acc8488f672325cd01d9e0f1bf5f09689c500
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-21 11:31:29 +0900

    add seeds structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Riddle.destroy_all
+
+riddle1 = Riddle.create(title: "", question: "", answer: "")
+riddle2 = Riddle.create(title: "", question: "", answer: "")
+riddle3 = Riddle.create(title: "", question: "", answer: "")
+riddle4 = Riddle.create(title: "", question: "", answer: "")
+riddle5 = Riddle.create(title: "", question: "", answer: "")
+riddle6 = Riddle.create(title: "", question: "", answer: "")
+riddle7 = Riddle.create(title: "", question: "", answer: "")
+riddle8 = Riddle.create(title: "", question: "", answer: "")
+riddle9 = Riddle.create(title: "", question: "", answer: "")
+riddle10 = Riddle.create(title: "", question: "", answer: "")
