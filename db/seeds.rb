bf846f336fac75187c432f5d50ee398511077270
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-21 14:21:12 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 
 Riddle.destroy_all
 
-riddle1 = Riddle.create(title: "", question: "", answer: "")
-riddle2 = Riddle.create(title: "", question: "", answer: "")
-riddle3 = Riddle.create(title: "", question: "", answer: "")
-riddle4 = Riddle.create(title: "", question: "", answer: "")
-riddle5 = Riddle.create(title: "", question: "", answer: "")
-riddle6 = Riddle.create(title: "", question: "", answer: "")
-riddle7 = Riddle.create(title: "", question: "", answer: "")
-riddle8 = Riddle.create(title: "", question: "", answer: "")
-riddle9 = Riddle.create(title: "", question: "", answer: "")
-riddle10 = Riddle.create(title: "", question: "", answer: "")
+riddle1 = Riddle.create(title: "Run run", question: "What can run but never walks?", answer: "The river!")
+riddle2 = Riddle.create(title: "Fly away", question: "What can fly but has no wings?", answer: "Time!")
+riddle3 = Riddle.create(title: "Flower", question: "What flower has 2 lips?", answer: "Tulips!")
+# riddle4 = Riddle.create(title: "", question: "", answer: "")
+# riddle5 = Riddle.create(title: "", question: "", answer: "")
+# riddle6 = Riddle.create(title: "", question: "", answer: "")
+# riddle7 = Riddle.create(title: "", question: "", answer: "")
+# riddle8 = Riddle.create(title: "", question: "", answer: "")
+# riddle9 = Riddle.create(title: "", question: "", answer: "")
+# riddle10 = Riddle.create(title: "", question: "", answer: "")
