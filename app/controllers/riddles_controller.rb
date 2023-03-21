a55d436790db6ac8b773ab8fc07c0c476a2b2fce
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-21 11:31:16 +0900

    add controller actions

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -1,2 +1,29 @@
 class RiddlesController < ApplicationController
+  def index
+    @riddles = Riddle.all
+    render json: @riddles
+  end
+
+  def show
+    @riddle = Riddle.find(params[:id])
+    render json: @forest
+  end
+
+  def create
+    @riddle = Riddle.create(title: params[:name], question: params[:question], answer: params[:answer])
+    render json: @riddle
+  end
+
+  def update
+    @riddle = Riddle.find(params[:id])
+    @riddle.update(title: params[:name], question: params[:question], answer: params[:answer])
+    render json: "#{@riddle.title} has been updated!"
+  end
+
+  def destroy
+    @riddle = Riddle.find(params[:id])
+    @riddle.destroy
+    render json: "#{@riddle.title} has been deleted!"
+  end
+
 end
